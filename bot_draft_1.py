aeb11fd08f347127c003b72754ededb0a2d2da47
simon <smurfylord67@gmail.com>
2022-07-17 20:28:33 -0700

    Changed a lot of what the bot says to the user. Added cancel and pomodoro method for info about the timer.

--- bot_draft_1.py
+++ bot_draft_1.py
@@ -1,12 +1,14 @@
 import discord
-from discord import Client
-from discord import guild
+from discord import Client, guild
 from interactions import Intents
 from threading import Thread
 from functions import pomodoro
 import asyncio
 
-HARRY_TOKEN = ""
+TOKEN = ""
+tasks = {}
+POMODORO_INFO = "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. \
+It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks."
 
 bot_intents = discord.Intents.all()
 bot = Client(intents=bot_intents)
@@ -18,14 +20,29 @@ async def on_ready():
 @bot.event
 async def on_message(msg) :
     message_sender = msg.author
+    user_id = message_sender.id
     if message_sender == bot.user : return
     message_string = msg.content
     if message_string.startswith("$study") :
-            asyncio.get_event_loop().create_task(pomodoro(msg))
-    
+            if user_id not in tasks :
+                timer = asyncio.get_event_loop().create_task(pomodoro(msg))
+                tasks[user_id] = timer
+            else :
+                await msg.reply("**" +message_sender.mention+ " Pomodoro Timer already set, type $cancel to stop your session.**")
     elif message_string.startswith("$clear") :
         await msg.channel.purge()
         await msg.channel.send("**Chat cleared.**")
 
+    elif message_string.startswith("$cancel") :
+        if user_id in tasks :
+            tasks[user_id].cancel()
+            tasks.pop(user_id)
+            await msg.reply("**" +message_sender.mention+ " Pomodoro Timer stopped.**")
+        else :
+            await msg.reply("**You aren't currently studying, type $study to start.**")
     
-bot.run(HARRY_TOKEN)
+    elif message_string.startswith("$pomodoro") :
+        await msg.reply("**" +message_sender.mention+ " " +POMODORO_INFO+ "**")
+
+    
+bot.run(TOKEN)
--- functions.py
+++ functions.py
@@ -7,15 +7,22 @@ LONG_BREAK = 5 #1200 # 20 MIN
 
 async def pomodoro(msg) :
     user_mention = msg.author.mention
-    for i in range(3) :
-        await msg.reply("**" +user_mention+ " start studying.**")
+    await msg.reply("**" +user_mention+ " Pomodoro Timer on. Start studying! (25 minutes)**")
+    await asyncio.sleep(STUDY_TIME) 
+    await msg.reply("**" +user_mention+ " first break time. (5 minutes)**")
+    await asyncio.sleep(SHORT_BREAK)
+
+    for i in range(2) :
+        await msg.reply("**" +user_mention+ " start studying again! (25 minutes)**")
         await asyncio.sleep(STUDY_TIME) 
-        await msg.reply("**" +user_mention+ " break time.**")
+        await msg.reply("**" +user_mention+ " break time. (5 minutes)**")
         await asyncio.sleep(SHORT_BREAK)
     
-    await msg.reply("**" +user_mention+ " start studying.**")
+    await msg.reply("**" +user_mention+ " start studying again! (25 minutes)**")
     await asyncio.sleep(STUDY_TIME)
-    await msg.reply("**" +user_mention+ " break time.**")
+    await msg.reply("**" +user_mention+ " last break time. (20 minutes)**")
+    await asyncio.sleep(LONG_BREAK)
+    await msg.reply("**" +user_mention+ " your study session is over, type $study to start again.**")
 
 
 
