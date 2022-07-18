1a8c307ba75ea7dbbe3030b15bdf4860e50f5b46
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2022-07-17 19:43:37 -0700

    Fixed a glitch with normal sleep by using asyncio sleep now will scale correctly and no longer freeze with multible users using it.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from time import sleep
+import asyncio
 
 STUDY_TIME = 5 #1500 # 25 MIN
 SHORT_BREAK = 5 #300 # 5 MIN
@@ -8,12 +9,12 @@ async def pomodoro(msg) :
     user_mention = msg.author.mention
     for i in range(3) :
         await msg.reply("**" +user_mention+ " start studying.**")
-        sleep(STUDY_TIME) 
+        await asyncio.sleep(STUDY_TIME) 
         await msg.reply("**" +user_mention+ " break time.**")
-        sleep(SHORT_BREAK)
+        await asyncio.sleep(SHORT_BREAK)
     
     await msg.reply("**" +user_mention+ " start studying.**")
-    sleep(STUDY_TIME)
+    await asyncio.sleep(STUDY_TIME)
     await msg.reply("**" +user_mention+ " break time.**")
 
 
