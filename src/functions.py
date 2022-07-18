750b6b5015e4d2093d9173e98ad29b48c13e64ed
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2022-07-17 22:20:12 -0700

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,9 @@
 from time import sleep
 import asyncio
 
-STUDY_TIME = 5 #1500 # 25 MIN
-SHORT_BREAK = 5 #300 # 5 MIN
-LONG_BREAK = 5 #1200 # 20 MIN
+STUDY_TIME = 1500 # 25 MIN
+SHORT_BREAK = 300 # 5 MIN
+LONG_BREAK = 1200 # 20 MIN
 
 async def pomodoro(msg,tasks) :
     user_mention = msg.author.mention
