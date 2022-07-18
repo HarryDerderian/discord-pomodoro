aa580658d06e12d18d30d5bf7fc83138131f79b9
s-ordunez <84251206+s-ordunez@users.noreply.github.com>
2022-07-17 21:26:26 -0700

    hide token

--- bot_draft_1.py
+++ bot_draft_1.py
@@ -4,8 +4,9 @@ from interactions import Intents
 from threading import Thread
 from functions import pomodoro
 import asyncio
+import json
 
-TOKEN = ""
+TOKEN = str(json.loads(token))
 tasks = {}
 POMODORO_INFO = "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. \
 It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks."
