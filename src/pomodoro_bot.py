c8ed21ee86a3acc7d406a5339688ff53fa053682
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2022-07-19 16:41:31 -0700

    Update pomodoro_bot.py

--- src/pomodoro_bot.py
+++ src/pomodoro_bot.py
@@ -7,7 +7,7 @@ import asyncio
 TOKEN = ""
 POMODORO_INFO = '''```diff
 - The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. 
-- It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks
+- It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks.
 ```'''
 COMMANDS =', '.join( ["$study", "$cancel", "$pomodoro", "$help"] )
 
